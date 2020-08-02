df09b17f9580f3e4fee0a9f164a113392f4099bc
Tamás <tszabad@gmail.com>
2020-08-02 20:07:20 +0200

    prediction added

--- movies_rating.py
+++ movies_rating.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-import numpy
+import numpy as np
 
 data = keras.datasets.imdb
 
@@ -16,17 +16,43 @@ word_index["<UNUSED>"] = 3
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
-def decode_review(text):
-	return " ".join([reverse_word_index.get(i, "?") for i in text])
 
 train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
 test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
 
+def decode_review(text):
+	return " ".join([reverse_word_index.get(i, "?") for i in text])
+
+
 model = keras.Sequential()
-model.add(keras.layers.Embedding(88000, 16))
+model.add(keras.layers.Embedding(10000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 
 model.summary()
 
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+
+print(results)
+
+test_review= test_data[0]
+predict = model.predict([test_review])
+print("Rewiew:")
+print(decode_review(test_review))
+print("Prediction: " + str(np.argmax(predict[0] )))
+print("Actual: " + str(test_labels[0]))
+print(results)
+
+model.save("model.h5")
+model = keras.models.load_model("model.h5")
